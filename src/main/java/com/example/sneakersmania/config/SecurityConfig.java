613b6afa79c9e490d9d298333f48d5500665331a
임빈영 <qlsdud960604@gmail.com>
2021-11-13 15:10:23 +0900

    회원가입 시 비밀번호 해시 처리

--- src/main/java/com/example/sneakersmania/config/SecurityConfig.java
+++ src/main/java/com/example/sneakersmania/config/SecurityConfig.java
@@ -1,21 +1,29 @@
 package com.example.sneakersmania.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity   // 스프링 시큐리티 필터가 스프링 필터 체인에 등록됨
 @EnableGlobalMethodSecurity(prePostEnabled = true)   // "@PreAuthorize" 애너테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/auth/**", "/js/**", "/css/**", "/image/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/auth/loginForm");
--- src/main/java/com/example/sneakersmania/service/UserService.java
+++ src/main/java/com/example/sneakersmania/service/UserService.java
@@ -1,8 +1,10 @@
 package com.example.sneakersmania.service;
 
+import com.example.sneakersmania.model.RoleType;
 import com.example.sneakersmania.model.User;
 import com.example.sneakersmania.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,8 +14,17 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     @Transactional
     public void join(User user) {
+        String rawPassword = user.getPassword();
+        String encodedPassword = encoder.encode(rawPassword);
+
+        user.setRole(RoleType.USER);
+        user.setPassword(encodedPassword);
+
         userRepository.save(user);
     }
 
